0f0b81e7e2b7ecba89e0f9e89fdac324d50e829d
ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2024-04-23 08:35:40 +0530

    format code

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,14 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(cors({
-    origin:'*'
-}))
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 
-app.get("/",(req,res)=> res.end("hello world"))
+app.get("/", (req, res) => res.end("hello world"));
 
-
-app.listen(8000, ()=>console.log("server running on 8000"))
+app.listen(8000, () => console.log("server running on 8000"));
