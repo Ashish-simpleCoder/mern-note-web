8bd27ec0cc8e83796330c83800ef158d08a76d72
ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2024-04-29 21:04:06 +0530

    feat: web - add secure axios instance for authorized api calls and setup react-query

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,16 @@
 import { Toaster } from 'react-hot-toast'
 import AppRouterProvider from './provider/router.provider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
 
 export default function App() {
    return (
       <>
-         <AppRouterProvider />
+         <QueryClientProvider client={queryClient}>
+            <AppRouterProvider />
+         </QueryClientProvider>
          <Toaster position='bottom-right' />
       </>
    )
--- apps/web/src/lib/axios-instance.ts
+++ apps/web/src/lib/axios-instance.ts
@@ -1,3 +1,10 @@
 import axios from 'axios'
+import { Cookie } from './cookie'
 
 export const AxiosInstance = axios.create({ baseURL: import.meta.env.VITE_APP_API_URL })
+export const SecureAxiosInstance = axios.create({
+   baseURL: import.meta.env.VITE_APP_API_URL,
+   headers: {
+      Authorization: 'Bearer ' + Cookie.get('access-token'),
+   },
+})
