a5b0f0177f46c253f8d6baad3f2e04201e43202f
ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2024-04-29 21:04:51 +0530

    fix: add token expiration error handling in auth-user middleware

--- apps/api/src/middleware/auth-user.ts
+++ apps/api/src/middleware/auth-user.ts
@@ -6,22 +6,22 @@ export function authUser(req: Request, res: Response, next: NextFunction) {
    const secret = process.env.TOKEN_SECRET || 'yoursecretkey'
    const token = req.headers?.authorization?.split(' ')?.[1]
    if (!token) {
-      return res.status(401).send({ status: "error", error: 'unauthorized user' })
+      return res.status(401).send({ status: 'error', error: 'unauthorized user' })
    }
 
    verify(token, secret, { complete: true }, (error, decoded_token) => {
-      if(error){
-         return res.status(401).json({ status: 'error', message: 'Session has been expired', name: error?.name });         
+      if (error) {
+         return res.status(401).json({ status: 'error', message: 'Session has been expired', name: error?.name })
          // if (error?.name == 'TokenExpiredError') {
          //    return res.status(401).json({ status: 'error', message: 'Session has been expired', name: error?.name });
-         // } 
+         // }
          // else if (error?.name == 'JsonWebTokenError') {
          //    return res.status(401).json({ status: 'error', message: '', name:error?.name });
          // }
          // return res.status(401).send({ status: "error", error: 'unauthorized user', errors: error })
       }
-      if (decoded_token){  
-         (req as Request & { user: User }).user = decoded_token.payload as User
+      if (decoded_token) {
+         ;(req as Request & { user: User }).user = decoded_token.payload as User
       }
       next()
    })
