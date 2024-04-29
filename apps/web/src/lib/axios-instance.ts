2948723a29490c337bae23296f23ccb69f1fe9cb
ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2024-04-29 21:57:55 +0530

    fix: web - use axios interceptor for protected api calls

--- apps/web/src/lib/axios-instance.ts
+++ apps/web/src/lib/axios-instance.ts
@@ -4,7 +4,8 @@ import { Cookie } from './cookie'
 export const AxiosInstance = axios.create({ baseURL: import.meta.env.VITE_APP_API_URL })
 export const SecureAxiosInstance = axios.create({
    baseURL: import.meta.env.VITE_APP_API_URL,
-   headers: {
-      Authorization: 'Bearer ' + Cookie.get('access-token'),
-   },
+})
+SecureAxiosInstance.interceptors.request.use((config) => {
+   config.headers.Authorization = 'Bearer ' + Cookie.get('access-token')
+   return config
 })
