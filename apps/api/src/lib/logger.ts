0b148cc4d4d6f57650714935274094bea54bb4a3
ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2024-05-13 08:47:22 +0530

    format code

--- apps/api/src/lib/logger.ts
+++ apps/api/src/lib/logger.ts
@@ -1,17 +1,16 @@
-import winston from 'winston';
+import winston from 'winston'
 
 const fileTransport = new winston.transports.File({
-    filename: 'logs/server.log',
-    format: winston.format.combine(
-        winston.format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
-        winston.format.printf(({ timestamp, level, message }) => {
-            return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-        })
-    )
-});
-
+   filename: 'logs/server.log',
+   format: winston.format.combine(
+      winston.format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
+      winston.format.printf(({ timestamp, level, message }) => {
+         return `${timestamp} [${level.toUpperCase()}]: ${message}`
+      })
+   ),
+})
 
 export const logger = winston.createLogger({
-    format: winston.format.json(),
-    transports: [fileTransport]
-});
+   format: winston.format.json(),
+   transports: [fileTransport],
+})
